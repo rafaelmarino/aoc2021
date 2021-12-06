3058c06fd64de2f19ce10fa51cf7b850e8a94542
Rafael Marino <rafaelmarinor@gmail.com>
2021-12-05 22:53:27 -0500

    add final refactor day5

--- code/day5.py
+++ code/day5.py
@@ -1,14 +1,59 @@
 #!/usr/bin/python3
 
 
+def move_vertically(x1, y1, y2, m_grid, e_grid):
+    """Move vertically across both grids."""
+    segment = y2 - y1
+    for step in range(abs(segment) + 1):
+        if segment > 0:
+            m_grid[y1 + step][x1] += 1
+            e_grid[y1 + step][x1] += 1
+        else:
+            m_grid[y1 - step][x1] += 1
+            e_grid[y1 - step][x1] += 1
+    return m_grid, e_grid
+
+
+def move_horizontally(x1, x2, y1, m_grid, e_grid):
+    """Move horizontally across both grids."""
+    segment = x2 - x1
+    for step in range(abs(segment) + 1):
+        if segment > 0:
+            m_grid[y1][x1 + step] += 1
+            e_grid[y1][x1 + step] += 1
+        else:
+            m_grid[y1][x1 - step] += 1
+            e_grid[y1][x1 - step] += 1
+    return m_grid, e_grid
+
+
+def move_diagonally(x1, x2, y1, y2, e_grid):
+    """Move diagonally across both grids."""
+    x_segment = x2 - x1
+    y_segment = y2 - y1
+    if x_segment > 0 and y_segment > 0:
+        for step in range(abs(x_segment) + 1):
+            e_grid[y1 + step][x1 + step] += 1
+    if x_segment > 0 and y_segment < 0:
+        for step in range(abs(x_segment) + 1):
+            e_grid[y1 - step][x1 + step] += 1
+    if x_segment < 0 and y_segment < 0:
+        for step in range(abs(x_segment) + 1):
+            e_grid[y1 - step][x1 - step] += 1
+    if x_segment < 0 and y_segment > 0:
+        for step in range(abs(x_segment) + 1):
+            e_grid[y1 + step][x1 - step] += 1
+    return e_grid
+
+
 def map_hydrothermal_vents(data):
     """--- Day 5: Hydrothermal Venture ---"""
     coordinates = []
     for line in data:
         from_, to_ = line.split(" -> ")[0], line.split(" -> ")[1]
-        from_x, from_y = from_.split(",")
-        to_x, to_y = to_.split(",")
-        coordinates.append(list(map(int, (from_x, from_y, to_x, to_y))))
+        x1, y1 = from_.split(",")
+        x2, y2 = to_.split(",")
+        coordinates.append(list(map(int, (x1, y1, x2, y2))))
     dim_length = max([max(row) for row in coordinates]) + 1  # 990 + 1
     # min([min(row) for row in coordinates])  # 10
     m_grid = [[0] * dim_length for i in range(0, dim_length)]  # manhattan
@@ -16,44 +61,18 @@ def map_hydrothermal_vents(data):
     for coords in coordinates:
         # print(coords)
         x1, y1, x2, y2 = coords[0], coords[1], coords[2], coords[3]
-        vertical = True if x1 - x2 == 0 else False
-        horizontal = True if y1 - y2 == 0 else False
-        diagonal = True if abs(x2 - x1) - abs(y2 - y1) == 0 else False
-        if vertical:  # constant x; iterate through rows
-            segment = y2 - y1
-            for step in range(abs(segment) + 1):
-                if segment > 0:
-                    m_grid[y1 + step][x1] += 1
-                    e_grid[y1 + step][x1] += 1
-                else:
-                    m_grid[y1 - step][x1] += 1
-                    e_grid[y1 - step][x1] += 1
-        if horizontal:  # constant y; iterate through row's cols
-            segment = x2 - x1
-            for step in range(abs(segment) + 1):
-                if segment > 0:
-                    m_grid[y1][x1 + step] += 1
-                    e_grid[y1][x1 + step] += 1
-                else:
-                    m_grid[y1][x1 - step] += 1
-                    e_grid[y1][x1 - step] += 1
-        if diagonal:  # iterate through both rows and cols
-            x_segment = x2 - x1
-            y_segment = y2 - y1
-            if x_segment > 0 and y_segment > 0:
-                for step in range(abs(x_segment) + 1):
-                    e_grid[y1 + step][x1 + step] += 1
-            if x_segment > 0 and y_segment < 0:
-                for step in range(abs(x_segment) + 1):
-                    e_grid[y1 - step][x1 + step] += 1
-            if x_segment < 0 and y_segment < 0:
-                for step in range(abs(x_segment) + 1):
-                    e_grid[y1 - step][x1 - step] += 1
-            if x_segment < 0 and y_segment > 0:
-                for step in range(abs(x_segment) + 1):
-                    e_grid[y1 + step][x1 - step] += 1
-    danger_areas_m = [1 if elem >= 2 else 0 for row in m_grid for elem in row]
-    danger_areas_e = [1 if elem >= 2 else 0 for row in e_grid for elem in row]
+        # check nature of movement (x1, y1) -> (x2, y2)
+        vertically = True if x1 - x2 == 0 else False
+        horizontally = True if y1 - y2 == 0 else False
+        diagonally = True if abs(x2 - x1) - abs(y2 - y1) == 0 else False
+        if vertically:  # constant x; iterate through rows (fixed x)
+            m_grid, e_grid = move_vertically(x1, y1, y2, m_grid, e_grid)
+        if horizontally:  # constant y; iterate through row's cols (fixed row)
+            m_grid, e_grid = move_horizontally(x1, x2, y1, m_grid, e_grid)
+        if diagonally:  # iterate through both rows and cols
+            e_grid = move_diagonally(x1, x2, y1, y2, e_grid)
+    danger_areas_m = [1 if el >= 2 else 0 for row in m_grid for el in row]
+    danger_areas_e = [1 if el >= 2 else 0 for row in e_grid for el in row]
     # return m_grid, e_grid, sum(danger_areas_m), sum(danger_areas_e)
     return sum(danger_areas_m), sum(danger_areas_e)
 
@@ -74,7 +93,6 @@ if __name__ == "__main__":
 #         else:
 #             grid = e_grid
 #         for row in grid:
-#             # grid[0]
 #             # f.write(str(row) + "\n")
 #             row = "".join(map(str, row))
 #             row = row.replace("0", ".")
