0ce0f0b8fc042e89cd58b8abcc3fb0b56751566c
Rafael Marino <rafaelmarinor@gmail.com>
2021-12-13 15:00:35 -0500

    push day13 refactor

--- code/day13.py
+++ code/day13.py
@@ -2,15 +2,13 @@
 
 import numpy as np
 
-if __name__ == "__main__":
-    test, actual = "test-input/day13.txt", "input/day13.txt"
-    with open(actual, "r") as f:
-        data = f.read().splitlines()
+
+def fold(data):
+    """--- Day 13: Transparent Origami ---"""
     paper = data[: data.index("")]
     paper = [list(map(int, coord.split(","))) for coord in paper]
     folds = data[data.index("") + 1 :]
-
-    for fold in folds:
+    for _, fold in enumerate(folds):
         max_x = max([coord[0] for coord in paper])
         max_y = max([coord[1] for coord in paper])
         dir, line = fold.split(" ")[-1].split("=")
@@ -24,23 +22,38 @@ if __name__ == "__main__":
             right = [(x, y) for x, y in paper if x > int(line)]
             to_add = [(max_x - x, y) for x, y in right]
             paper = set(left) | set(to_add)
+        if _ == 0:
+            part1 = len(paper)
+    return part1, paper
 
-    len(paper)
 
+def create_grid(paper):
+    """--- Part Two ---"""
     max_x = max([coord[0] for coord in paper])
     max_y = max([coord[1] for coord in paper])
     grid = np.zeros((max_y + 1, max_x + 1)).astype(int)
     for coord in paper:
         y, x = coord
         grid[x, y] = 1
+    return grid
 
-    def write_txt(file, path):
-        """Write eight capital letters to text"""
-        with open(path, "w") as f:
-            for row in file:
-                row = "".join(map(str, row))
-                row = row.replace("0", " ")
-                row = row.replace("1", "#")
-                _ = f.write(row + "\n")
 
-    write_txt(grid, "extras/day13.txt")
+def write_txt(data, path):
+    """Write eight capital letters to text"""
+    with open(path, "w") as f:
+        for row in data:
+            row = "".join(map(str, row))
+            row = row.replace("0", " ")
+            row = row.replace("1", "#")
+            _ = f.write(row + "\n")
+
+
+if __name__ == "__main__":
+    test, actual = "test-input/day13.txt", "input/day13.txt"
+    with open(actual, "r") as f:
+        data = f.read().splitlines()
+    p1, folded_paper = fold(data)
+    grid = create_grid(folded_paper)
+    # write_txt(grid, "extras/day13.txt")
+    print(f"Part 1 -- Dots after the first fold in the paper: {p1}")
+    print(f"Part 2 -- Dots after all the folds: {len(folded_paper)}")
